df5e83b78956960dc07be42dcd085112a7f41aaa
stephen <fmarsf@gmail.com>
2014-09-05 15:18:51 -0700

    mongodb

---
+++ MongoDB/MyTest/insert.h
@@ -0,0 +1,14 @@
+#include<string>
+using namespace std;
+const string db_name("fmars");
+const string co_name("test");
+const string dot_name(".");
+const string fu_name=db_name+dot_name+co_name;
+
+const int insert_record_num=1000000;
+
+
+const bool clear_previous_data=1;
+const bool insert_records=1;
+const bool print_all_the_record=1;
+const bool do_single_query=0;
